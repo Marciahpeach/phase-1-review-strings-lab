931b7930121b9a6bfd32bef69a259167aa1c5b1d
Marciah <marciah.machoka@student.moringaschool.com>
2025-03-05 23:59:58 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+const currentUser = 'Marciah';
+
+const welcomeMessage = 'Welcome to Flatbook, Marciah!';
+
+//const excitedWelcomeMessage ='WELCOME TO FLATBOOK';
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, MARCIAH!';
+
+//const shortGreeting = "Welcome, ";
+const shortGreeting = "Welcome, M!";
